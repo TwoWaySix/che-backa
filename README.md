6381366537c18e90e94c697bab02cfc073d28aa0
Circe <paz@twowaysix.com>
2022-05-17 21:25:03 +0200

    started implementing incremental backups

--- README.md
+++ README.md
@@ -6,12 +6,10 @@ An automated simple yet powerful backup solution that checks the integrity of th
 
 
 ## Concept
-- Allows daily, weekly and monthly versioned backups
-- Before each backup, all the files are compared to the newest backup
-- MD5 hashes and file meta data are compared
+- incremental backups with hard links
+- MD5 hashes are compared
 - There are three possible outcomes:
   1. MD5 and meta data **the same** -> No action required, because it is the same file.
   2. MD5 and meta data **different** -> File will be versioned, because some changes have happened to it.
   3. MD5 **different** and meta data **the same** -> Possible hard drive corruption. File will be versioned. Since it might be a drive issue, it will be reported.
-- Versioning means appending "_v%0d" to the file name
 - The meta data used are **file size** and **edit date**
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,90 @@
 use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, SystemTime};
+use chrono::Utc;
 
+struct Config {
+    backup_src: PathBuf,
+    backup_dest: PathBuf,
+    dt_backup_hours: f64
+}
 
 fn main() {
+    // TODO: Get these values from command line args
+    let backup_src = PathBuf::from("./data/data");
+    let backup_dest = PathBuf::from("./data/backups");
+    let dt_backup_hours = 0.01;
+
+    let cfg = Config{
+        backup_src: backup_src,
+        backup_dest: backup_dest,
+        dt_backup_hours: dt_backup_hours
+    };
+    
+    start_program_loop(&cfg);
+    
+}
+
+fn start_program_loop(cfg: &Config) {
+    loop {
+        create_backup(cfg);
+        
+        let dur = Duration::from_secs(
+            (cfg.dt_backup_hours * 3600.) as u64
+        );
+        std::thread::sleep(dur);
+    }
+}
+
+fn create_backup(cfg: &Config) {
+    let new_backup_directory = create_new_backup_directory(cfg);
+        
+    recursively_iterate_files(&cfg.backup_src, &new_backup_directory);
+}
+
+fn create_new_backup_directory(cfg: &Config) -> PathBuf {
+    let now = Utc::now();
+    let new_dir_name = now.format("%Y%m%d_%H%M%S").to_string();
+    println!("{}", new_dir_name);
+
+    let new_backup_directory = cfg.backup_dest.join(new_dir_name);
+    println!("{:?}", new_backup_directory);
+    fs::create_dir(new_backup_directory.as_path()).unwrap(); // TODO: Remove unwrap
+
+    new_backup_directory
+}
+
+fn recursively_iterate_files(source_directoy: &Path, backup_directory: &Path) {
+    for res in fs::read_dir(source_directoy).unwrap() { // TODO: Remove unwrap
+        let dir_entry = res.unwrap(); // TODO: Remove unwrap
+        let path = dir_entry.path();
+
+        if path.is_file() {
+            match backup_file(&path, backup_directory) {
+                Ok(a) => a,
+                Err(e) => println!("WARNING: Could not backup file {:?}: {:?}", path, e)
+            };
+        } else if path.is_dir() {
+            recursively_iterate_files(&path, backup_directory);
+        } else {
+            println!("WARNING: Encountered neither file nor dir: {:?}", path);
+        }
+    }
+}
+
+fn backup_file(path: &Path, backup_directory: &Path) -> std::io::Result<()> {
+    let new_path = backup_directory.join(path.clone());
+    let new_dir = new_path.parent().unwrap_or(Path::new("/")); // TODO: Is that really the solution?
+    // Creating new directory structure
+    fs::create_dir_all(new_dir)?;
+    // Copying file to new directory structure
+    fs::copy(path, new_path)?;
+    Ok(())
+}
+
+fn testing() {
     let path = Path::new("data/image.jpeg");
     let display = path.display();
     let meta = std::fs::metadata(path).unwrap();
